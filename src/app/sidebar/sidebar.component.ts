c1523476a28648368df5a4f16e05a1b4e04a4d75
pavelkireev <pkireev97@gmail.com>
2023-11-23 02:31:19 +0100

    small fixes

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -7,17 +7,32 @@ import {AuthService} from "../service/auth.service";
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-  public sidebarLinks = [
-    {route: '/my-profile', imgURL: '/assets/assets/user.svg', name: 'Profile'},
-    {route: '/working-hours', imgURL: '/assets/assets/timer.svg', name: 'Working Hours'},
-    {route: '/appointment', imgURL: '/assets/assets/calendar.svg', name: 'Appointments'},
-    {route: '/doctor', imgURL: '/assets/assets/doctors.svg', name: 'Doctors'},
-    {route: '/patient', imgURL: '/assets/assets/members.svg', name: 'Patients'},
-    {route: '/create-user', imgURL: '/assets/assets/plus.svg', name: 'Create User'},
-  ];
-
+  public sidebarLinks : any[] = [];
 
   constructor(private authService: AuthService) {
+
+      if (authService.isAdmin()) {
+          this.sidebarLinks = [
+              {route: '/my-profile', imgURL: '/assets/assets/user.svg', name: 'Profile'},
+              {route: '/appointment', imgURL: '/assets/assets/calendar.svg', name: 'Appointments'},
+              {route: '/doctor', imgURL: '/assets/assets/doctors.svg', name: 'Doctors'},
+              {route: '/patient', imgURL: '/assets/assets/members.svg', name: 'Patients'},
+              {route: '/create-user', imgURL: '/assets/assets/plus.svg', name: 'Create User'}
+          ];
+      } else if (authService.isDoctor()) {
+          this.sidebarLinks = [
+              {route: '/my-profile', imgURL: '/assets/assets/user.svg', name: 'Profile'},
+              {route: '/working-hours', imgURL: '/assets/assets/timer.svg', name: 'Working Hours'},
+              {route: '/appointment', imgURL: '/assets/assets/calendar.svg', name: 'Appointments'},
+              {route: '/patient', imgURL: '/assets/assets/members.svg', name: 'Patients'}
+          ];
+      } else {
+          this.sidebarLinks = [
+              {route: '/my-profile', imgURL: '/assets/assets/user.svg', name: 'Profile'},
+              {route: '/appointment', imgURL: '/assets/assets/calendar.svg', name: 'Appointments'},
+          ];
+      }
+
   }
 
   get isUserAuthenticated(): boolean {
@@ -32,6 +47,6 @@ export class SidebarComponent implements OnInit {
     return (currentPath.includes(linkRoute) && linkRoute.length > 1) || currentPath === linkRoute;
   }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+  }
 }
