500409b4a32f7b9f7551df7ffc8f2ca86d197b21
returnT0 <vlvolkov25@gmail.com>
2023-12-11 17:49:13 +0100

    last adjustments

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -15,49 +15,47 @@ export class SidebarComponent implements OnInit {
               private router: Router,
               private chatService: ChatService
   ) {
-      if (authService.isAdmin()) {
-          this.sidebarLinks = [
-              {route: '/my-profile', imgURL: '/assets/assets/user.svg', name: 'Profile'},
-              {route: '/appointment', imgURL: '/assets/assets/calendar.svg', name: 'Appointments'},
-              {route: '/create-user', imgURL: '/assets/assets/plus.svg', name: 'Create User'},
-              {route: '/list/doctor', imgURL: '/assets/assets/doctors.svg', name: 'Doctors'},
-              {route: '/list/patient', imgURL: '/assets/assets/members.svg', name: 'Patients'},
-              {route: '/inbox', imgURL: '/assets/assets/letter.svg', name: 'Inbox'},
-              {route: '/outbox', imgURL: '/assets/assets/pen.svg', name: 'Outbox'}
-          ];
-      } else if (authService.isDoctor()) {
-          this.sidebarLinks = [
-              {route: '/my-profile', imgURL: '/assets/assets/user.svg', name: 'Profile'},
-              {route: '/working-hours', imgURL: '/assets/assets/timer.svg', name: 'Working Hours'},
-              {route: '/appointment', imgURL: '/assets/assets/calendar.svg', name: 'Appointments'},
-              {route: '/list/patient', imgURL: '/assets/assets/members.svg', name: 'Patients'},
-              {route: '/inbox', imgURL: '/assets/assets/unread.svg', name: 'Inbox'},
-              {route: '/inbox', imgURL: '/assets/assets/letter.svg', name: 'Inbox'},
-              {route: '/outbox', imgURL: '/assets/assets/pen.svg', name: 'Outbox'}
-          ];
-      } else {
-          this.sidebarLinks = [
-              {route: '/my-profile', imgURL: '/assets/assets/user.svg', name: 'Profile'},
-              {route: '/appointment', imgURL: '/assets/assets/calendar.svg', name: 'Appointments'},
-              {route: '/inbox', imgURL: '/assets/assets/unread.svg', name: 'Inbox'},
-              {route: '/inbox', imgURL: '/assets/assets/letter.svg', name: 'Inbox'},
-              {route: '/outbox', imgURL: '/assets/assets/pen.svg', name: 'Outbox'}
-          ];
-      }
+    if (authService.isAdmin()) {
+      this.sidebarLinks = [
+        {route: '/my-profile', imgURL: '/assets/assets/user.svg', name: 'Profile'},
+        {route: '/appointment', imgURL: '/assets/assets/calendar.svg', name: 'Appointments'},
+        {route: '/create-user', imgURL: '/assets/assets/plus.svg', name: 'Create User'},
+        {route: '/list/doctor', imgURL: '/assets/assets/doctors.svg', name: 'Doctors'},
+        {route: '/list/patient', imgURL: '/assets/assets/members.svg', name: 'Patients'},
+        {route: '/inbox', imgURL: '/assets/assets/letter.svg', name: 'Inbox'},
+        {route: '/outbox', imgURL: '/assets/assets/pen.svg', name: 'Outbox'}
+      ];
+    } else if (authService.isDoctor()) {
+      this.sidebarLinks = [
+        {route: '/my-profile', imgURL: '/assets/assets/user.svg', name: 'Profile'},
+        {route: '/working-hours', imgURL: '/assets/assets/timer.svg', name: 'Working Hours'},
+        {route: '/appointment', imgURL: '/assets/assets/calendar.svg', name: 'Appointments'},
+        {route: '/list/patient', imgURL: '/assets/assets/members.svg', name: 'Patients'},
+        {route: '/inbox', imgURL: '/assets/assets/letter.svg', name: 'Inbox'},
+        {route: '/outbox', imgURL: '/assets/assets/pen.svg', name: 'Outbox'}
+      ];
+    } else {
+      this.sidebarLinks = [
+        {route: '/my-profile', imgURL: '/assets/assets/user.svg', name: 'Profile'},
+        {route: '/appointment', imgURL: '/assets/assets/calendar.svg', name: 'Appointments'},
+        {route: '/inbox', imgURL: '/assets/assets/letter.svg', name: 'Inbox'},
+        {route: '/outbox', imgURL: '/assets/assets/pen.svg', name: 'Outbox'}
+      ];
+    }
 
-      this.chatService.notifyNewMessagesAvailable.subscribe((newMessagesAvailable: boolean) => {
-        if (newMessagesAvailable) {
-          this.sidebarLinks.forEach(link => {
-            link.imgURL = link.imgURL === '/assets/assets/letter.svg' ? '/assets/assets/unread.svg'
-                                                                      : link.imgURL
-          });
-        } else {
-            this.sidebarLinks.forEach(link => {
-              link.imgURL = link.imgURL === '/assets/assets/unread.svg' ? '/assets/assets/letter.svg'
-                                                                        : link.imgURL
-            });
-        }
-      });
+    this.chatService.notifyNewMessagesAvailable.subscribe((newMessagesAvailable: boolean) => {
+      if (newMessagesAvailable) {
+        this.sidebarLinks.forEach(link => {
+          link.imgURL = link.imgURL === '/assets/assets/letter.svg' ? '/assets/assets/unread.svg'
+            : link.imgURL
+        });
+      } else {
+        this.sidebarLinks.forEach(link => {
+          link.imgURL = link.imgURL === '/assets/assets/unread.svg' ? '/assets/assets/letter.svg'
+            : link.imgURL
+        });
+      }
+    });
   }
 
   get isUserAuthenticated(): boolean {
